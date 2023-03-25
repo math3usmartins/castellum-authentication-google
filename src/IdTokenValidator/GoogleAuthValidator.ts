11d6d963eb779a3d76aff4940c9f5c26a8973007
Matheus Martins <mathrm@gmail.com>
2023-03-25 01:50:11 +0100

    implement automated test to verify token
    * implement automated test to verify token
    
    * update package version to 0.0.3

--- src/IdTokenValidator/GoogleAuthValidator.ts
+++ src/IdTokenValidator/GoogleAuthValidator.ts
@@ -6,10 +6,9 @@ export class GoogleAuthValidator implements IdTokenValidator {
 	constructor(private readonly clientId: string, private readonly client: OAuth2Client) {}
 
 	public async validate(token: string, email: string): Promise<void> {
-		const ticket = await this.client.verifyIdToken({
-			idToken: token,
-			audience: this.clientId,
-		})
+		const ticket = await this.client
+			.verifyIdToken({ idToken: token, audience: this.clientId })
+			.catch(async () => await Promise.reject(new BadTokenError()))
 
 		const payload = ticket.getPayload()
 
